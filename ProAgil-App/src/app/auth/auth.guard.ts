6ef2197fdee96b7fd14cd56081dede4a9042d50b
Marconcilio Das Virgens de Souza <marconcilio.souza@terceiroslinx.com.br>
2020-03-31 18:27:44 -0300

    correção auth

--- ProAgil-App/src/app/auth/auth.guard.ts
+++ ProAgil-App/src/app/auth/auth.guard.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-      if (localStorage.getItem('token') !== null) {
-          return true;
-      } else {
-        this.router.navigate(['/user/login']);
-        return false;
-      }
-  }
 
   constructor(private router: Router) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (localStorage.getItem('token') !== null) {
+      return true;
+    } else {
+      this.router.navigate(['/user/login']);
+      return false;
+    }
+  }
 }
